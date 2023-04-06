8b5cf597fa87cea7a0e58596d46f4156b974c36f
sometimemecry <songphon.yy@hotmail.com>
2023-04-06 20:16:07 +0700

    Test Commit

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import './nav.css'
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BiBook} from 'react-icons/bi'
+import {RiServiceLine} from 'react-icons/ri'
+import {RiContactsLine} from 'react-icons/ri'
+import {useState} from 'react'
 
 const nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
-    <div>nav</div>
+    <nav>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#' ? 'active' : ''}><AiOutlineUser /></a>
+      <a href="#experience"><BiBook /></a>
+      <a href="#services"><RiServiceLine /></a>
+      <a href="#contact"><RiContactsLine /></a>
+    </nav>
   )
 }
 
