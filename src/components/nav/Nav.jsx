e6bb85dde9f6a84bbbb08326193d50e4680624ba
sometimemecry <songphon.yy@hotmail.com>
2023-04-10 20:51:25 +0700

    Add adjust active Nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -7,17 +7,17 @@ import {RiServiceLine} from 'react-icons/ri'
 import {RiContactsLine} from 'react-icons/ri'
 import {useState} from 'react'
 
-const nav = () => {
+const Nav = () => {
   const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
       <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
-      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#' ? 'active' : ''}><AiOutlineUser /></a>
-      <a href="#experience"><BiBook /></a>
-      <a href="#services"><RiServiceLine /></a>
-      <a href="#contact"><RiContactsLine /></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook /></a>
+      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine /></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><RiContactsLine /></a>
     </nav>
   )
 }
 
-export default nav
+export default Nav
