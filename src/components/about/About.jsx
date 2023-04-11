4036b5416ae7ab70ae5cea41f4cce76e26dbc033
sometimemecry <songphon.yy@hotmail.com>
2023-04-11 18:57:48 +0700

    About Component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,53 @@
-import React from 'react'
-import './about.css'
+import React from "react";
+import "./about.css";
+import ME from "../../assets/me.jpg";
+import { FaAward } from "react-icons/fa";
+import {FiUsers} from "react-icons/fi"
+import {VscFolderLibrary} from "react-icons/vsc"
 
-const about = () => {
+const About = () => {
   return (
-    <section id='about'>About</section>
-  )
-}
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
 
-export default about
+      <div className="container about_container">
+        <div className="about_me">
+          <div className="about_me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+        <div className="about_content">
+          <div className="about_cards">
+
+            <article className="about_card">
+              <FaAward className="about_icon"/>
+              <h5>Experience</h5>
+              <small>2+ Years Working</small>
+            </article>
+
+            <article className="about_card">
+              <FiUsers className="about_icon"/>
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+
+            <article className="about_card">
+              <VscFolderLibrary className="about_icon"/>
+              <h5>Project</h5>
+              <small>80+ Completed</small>
+            </article>
+          </div>
+
+          <p>
+            lorem ipsum dolor sit amet, consectetur adip
+          </p>
+
+          <a href="#contact" className="btn btn-primary">Let's Talk</a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
